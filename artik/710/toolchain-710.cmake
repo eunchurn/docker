7e1c1060c72aee915d0de2d43126e88824340a63
Kapil Sachdeva <ksachdeva17@gmail.com>
2018-02-26 14:54:33 -0600

    add the Docker file for putting the ml libraries (dlib to start with) in ARTIK 710

--- artik/710/toolchain-710.cmake
+++ artik/710/toolchain-710.cmake
@@ -4,6 +4,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(cross_triple "aarch64-linux-gnu")
 
+set(CMAKE_C_COMPILER /arxc/gcc-linaro-aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /arxc/gcc-linaro-aarch64-linux-gnu/bin/aarch64-linux-gnu-g++)
 
 # Discard path returned by pkg-config and associated with HINTS in module
